77833c2d880b2fde49cf6e335021bc0ee7db7ee0
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-07-05 09:57:46 -0700

    modified posts/index, partials/navbar, and the show post view page

--- src/main/java/com/codeup/spring_blog/controllers/PostController.java
+++ src/main/java/com/codeup/spring_blog/controllers/PostController.java
@@ -1,37 +1,48 @@
 package com.codeup.spring_blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 
+@Controller
 public class PostController {
 
 
-    @GetMapping(path = "/posts")
-    @ResponseBody
-    public String index() {
-        return "posts";
+    @GetMapping(path = "/posts/index")
+//    @ResponseBody
+    public String index(Model model) {
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        model.addAttribute("posts", posts);
+
+        return "posts/index";
     }
 
-    @GetMapping(path = "/posts/{id}")
-    @ResponseBody
-    public int individualPost(@PathVariable int id) {
-        return id;
-    }
 
-    @GetMapping(path = "/posts/create")
-    @ResponseBody
-    public String createPost() {
-        return "createPosts";
 
-    }
-
-    @GetMapping("/index")
-    @ResponseBody
-    public String indexPage(){
-    return "index";
-    }
-
-    @GetMapping("/show")
+//    @GetMapping(path = "/posts/{id}")
+//    @ResponseBody
+//    public int individualPost(@PathVariable int id) {
+//        return id;
+//    }
+//
+//    @GetMapping(path = "/posts/create")
+//    @ResponseBody
+//    public String createPost() {
+//        return "createPosts";
+//
+//    }
+//
+//    @GetMapping("/index")
+//    @ResponseBody
+//    public String indexPage(){
+//    return "index";
+//    }
+//
+    @GetMapping("/posts")
     @ResponseBody
     public String showPage(){
         return "show";
