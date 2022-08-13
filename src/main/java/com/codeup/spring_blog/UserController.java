279b8e1478d4cf12eac8cc697213dd6efe78af0f
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-08-13 16:52:52 -0700

    created edit user functionality in the user controller and created a user edit view. verified that it works

--- src/main/java/com/codeup/spring_blog/UserController.java
+++ src/main/java/com/codeup/spring_blog/UserController.java
@@ -1,11 +1,13 @@
 package com.codeup.spring_blog;
 import com.codeup.spring_blog.User;
 import com.codeup.spring_blog.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -37,6 +39,26 @@ public class UserController {
     public String userLogout() {
         return "redirect:/login";
     }
+
+    @GetMapping("/users/edit/{id}")
+    public String editUser(@PathVariable long id, Model model) {
+        model.addAttribute("user", userRepository.getById(id));
+        model.addAttribute("currentUser", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return "users/edit";
+    }
+
+    @PostMapping("users/edit")
+    public String editListing(@ModelAttribute User user){
+        String hash = encoder.encode(user.getPassword());
+        user.setPassword(hash);
+        System.out.println(user.getPassword());
+        userRepository.save(user);
+        return "redirect:users/profile";
+    }
+
+
+
+
 
 
 
